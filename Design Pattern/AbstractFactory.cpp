06a47687688a0921255d47236712f22b4a02c260
rajaram <rajaramsharma422@gmail.com>
2024-02-12 00:24:58 +0530

    added smart pointer

--- Design Pattern/AbstractFactory.cpp
+++ Design Pattern/AbstractFactory.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include<memory>
 using namespace std;
 
 class IProductA
@@ -11,7 +12,7 @@ public:
 class ProductA1 : public IProductA
 {
 public:
-    string usefulFun() const override
+    string usefulFun() const 
     {
         return "result of ProductA1";
     }
@@ -19,7 +20,7 @@ public:
 class ProductA2 : public IProductA
 {
 public:
-    string usefulFun() const override
+    string usefulFun() const 
     {
         return "result of ProductA2";
     }
@@ -33,7 +34,7 @@ public:
 class ProductB1 : public IProductB
 {
 public:
-    string usefulFun() const override
+    string usefulFun() const 
     {
         return "result of ProductB1";
     }
@@ -41,7 +42,7 @@ public:
 class ProductB2 : public IProductB
 {
 public:
-    string usefulFun() const override
+    string usefulFun() const 
     {
         return "result of ProductB2";
     }
@@ -51,38 +52,38 @@ class IFactory
 {
 public:
     virtual ~IFactory() {}
-    virtual IProductA *createProductA() const = 0;
-    virtual IProductB *createProductB() const = 0;
+    virtual shared_ptr<IProductA> createProductA() const = 0;
+    virtual shared_ptr<IProductB> createProductB() const = 0;
 };
 
 class Factory1 : public IFactory
 {
-    IProductA *createProductA() const override
+    shared_ptr<IProductA> createProductA() const 
     {
-        return new ProductA1();
+        return make_shared<ProductA1>();
     }
-    IProductB *createProductB() const override
+    shared_ptr<IProductB> createProductB() const 
     {
-        return new ProductB1();
+        return make_shared<ProductB1>();
     }
 };
 
 class Factory2 : public IFactory
 {
-    IProductA *createProductA() const override
+    shared_ptr<IProductA> createProductA() const 
     {
-        return new ProductA2();
+        return make_shared<ProductA2>();
     }
-    IProductB *createProductB() const override
+    shared_ptr<IProductB> createProductB() const 
     {
-        return new ProductB2();
+        return make_shared<ProductB2>();
     }
 };
 
-void clientCode(IFactory *factory)
+void clientCode(shared_ptr<IFactory> factory)
 {
-    IProductA *productA = factory->createProductA();
-    IProductB *productB = factory->createProductB();
+    shared_ptr<IProductA> productA = factory->createProductA();
+    shared_ptr<IProductB> productB = factory->createProductB();
     cout << productA->usefulFun() << "\n";
     cout << productB->usefulFun() << "\n";
 }
@@ -90,12 +91,10 @@ void clientCode(IFactory *factory)
 int main()
 {
     cout << "Factory 1 :\n";
-    IFactory *f1 = new Factory1();
+    shared_ptr<IFactory> f1 = make_shared<Factory1>();
     clientCode(f1);
-    delete f1;
 
     cout << "Factory 2 :\n";
-    IFactory *f2 = new Factory2();
+    shared_ptr<IFactory> f2 = make_shared<Factory2>();
     clientCode(f2);
-    delete f2;
 }
